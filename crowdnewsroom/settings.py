21d301c49091d6eb722df003767bdd74a1afc151
Knut Hühne <knut.huehne@correctiv.org>
2018-02-08 11:29:26 +0100

    load `ALLOWED_HOSTS` from environment variables

--- crowdnewsroom/settings.py
+++ crowdnewsroom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', "").split(",")
 
 
 # Application definition
