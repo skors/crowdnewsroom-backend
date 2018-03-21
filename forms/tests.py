7e0352cf8df744979380e01d77a624e0f6f65954
Knut Hühne <knut.huehne@correctiv.org>
2018-03-21 13:34:10 +0100

    add basic test

--- forms/tests.py
+++ forms/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from forms.models import Investigation, UserGroup
 
-# Create your tests here.
+
+class UserGroupTestCase(TestCase):
+    def test_create_groups(self):
+        user_groups = UserGroup.objects.all()
+        self.assertEqual(len(user_groups), 0, "at the beginning there should be no groups")
+
+        Investigation.objects.create(name="example investigation")
+
+        user_groups = UserGroup.objects.all()
+        self.assertEqual(len(user_groups), 5, "should create one group for each role")
