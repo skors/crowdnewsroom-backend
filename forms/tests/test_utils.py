2ce5b9192dd93086364413c9ce23822756837dae
Knut Hühne <knut.huehne@correctiv.org>
2018-04-23 18:11:08 +0200

    use `rendered_fields` to generate csv

--- forms/tests/test_utils.py
+++ forms/tests/test_utils.py
@@ -1,15 +1,13 @@
 from io import StringIO
-from unittest.mock import Mock, MagicMock
 
 import pytz
 from django.test import TestCase
 from django.utils.datetime_safe import datetime
 from django.utils.text import slugify
-from webob import Request
 
-from forms.models import Investigation, Form, FormInstance, FormResponse
+from forms.models import Investigation, Form, FormInstance
 from forms.tests.factories import FormResponseFactory
-from forms.utils import _get_file_keys, create_form_csv
+from forms.utils import create_form_csv
 
 
 class Utils(TestCase):
@@ -21,66 +19,6 @@ class Utils(TestCase):
         self.form = Form.objects.create(name=form_name,
                                         slug=slugify(form_name),
                                         investigation=self.investigation)
-
-    def test_get_file_keys_finds_file(self):
-        form_instance = FormInstance.objects.create(form=self.form,
-                                                    form_json=[{
-                                                        "schema": {
-                                                            "slug": "first",
-                                                            "properties": {
-                                                                "string": {"type": "string"},
-                                                                "file": {"type": "string",
-                                                                         "format": "data-url"}
-                                                            }
-                                                        }
-                                                    }])
-
-        expected = {"file"}
-        file_keys = _get_file_keys(form_instance)
-        self.assertEqual(file_keys, expected)
-
-    def test_get_file_keys_finds_file_array(self):
-        form_instance = FormInstance.objects.create(form=self.form,
-                                                    form_json=[{
-                                                        "schema": {
-                                                            "slug": "first",
-                                                            "properties": {
-                                                                "files": {
-                                                                    "type": "array",
-                                                                    "items": {
-                                                                        "type": "string",
-                                                                        "format": "data-url"
-                                                                    }
-                                                                },
-                                                            }
-                                                        }
-                                                    }])
-
-        expected = {"files"}
-        file_keys = _get_file_keys(form_instance)
-        self.assertEqual(file_keys, expected)
-
-    def test_get_file_keys_finds_signature(self):
-        form_instance = FormInstance.objects.create(form=self.form,
-                                                    form_json=[{
-                                                        "schema": {
-                                                            "slug": "first",
-                                                            "properties": {
-                                                                "signature": {"type": "string"},
-                                                            }
-                                                        }
-                                                    }],
-                                                    ui_schema_json={
-                                                        "first": {
-                                                            "signature": {
-                                                                "ui:widget": "signatureWidget"
-                                                            }
-                                                        }
-                                                    })
-
-        expected = {"signature"}
-        file_keys = _get_file_keys(form_instance)
-        self.assertEqual(file_keys, expected)
 
     def test_create_form_csv(self):
         buffer = StringIO()
@@ -122,3 +60,44 @@ class Utils(TestCase):
         second = lines[2].strip()
         expected_second = "katharina@example.com,Submitted,2018-01-02 00:00:00+00:00,http://example.com,0,Katharina"
         self.assertEquals(second, expected_second)
+
+    def test_create_form_csv_file(self):
+        buffer = StringIO()
+        build_absolute_uri = lambda x: "https://example.com{}".format(x)
+
+        form_instance = FormInstance.objects.create(form=self.form,
+                                                    form_json=[{
+                                                        "schema": {
+                                                            "slug": "first",
+                                                            "properties": {
+                                                                "name": {"type": "string"},
+                                                                "picture": {"type": "string",
+                                                                            "format": "data-url"},
+                                                            }
+                                                        }
+                                                    }])
+
+        response = FormResponseFactory.create(form_instance=form_instance,
+                                   email="katharina@example.com",
+                                   submission_date=datetime(2018, 1, 2, tzinfo=pytz.utc),
+                                   json={
+                                       "name": "Katharina",
+                                       "picture": "data-url...."
+                                   })
+        create_form_csv(form_instance.form, form_instance.form.investigation.slug, build_absolute_uri, buffer)
+        lines = buffer.getvalue().split('\n')
+
+        header = lines[0].strip()
+        expected_header = "meta_email,meta_status,meta_submission_date,meta_url,meta_version,name,picture"
+        self.assertEquals(header, expected_header)
+
+        first = lines[1].strip()
+        expected_first = ",".join(["katharina@example.com",
+                         "Submitted",
+                         "2018-01-02 00:00:00+00:00",
+                         "https://example.com/forms/admin/investigations/first-investigation/forms/first-form/responses/{}".format(response.id),
+                         "0",
+                         "Katharina",
+                         "https://example.com/forms/admin/investigations/first-investigation/forms/first-form/responses/{}/files/picture".format(response.id)])
+        self.assertEquals(first, expected_first)
+
--- forms/utils.py
+++ forms/utils.py
@@ -9,15 +9,22 @@ def create_form_csv(form, investigation_slug, build_absolute_uri, io_object):
     form_instances = FormInstance.objects.filter(form_id=form.id)
     responses = FormResponse.objects.filter(form_instance__form_id=form.id).order_by("id").all()
 
-    fields = set()
+    extra_fields = {"url", "version", "status", "email", "submission_date"}
+    fields = {"meta_{}".format(field) for field in extra_fields}
     for instance in form_instances:
-        fields |= get_keys(instance)
+        fields |= instance.json_properties
 
     writer = csv.DictWriter(io_object, fieldnames=sorted(fields), extrasaction='ignore')
     writer.writeheader()
     for form_response in responses:
         try:
-            row = form_response.json
+            row = {}
+            for field in form_response.rendered_fields():
+                if field['type'] == "link":
+                    row[field["json_name"]] = build_absolute_uri(field["value"])
+                else:
+                    row[field["json_name"]] = field["value"]
+
             path = reverse("response_details", kwargs={"investigation_slug": investigation_slug,
                                                        "form_slug": form.slug,
                                                        "response_id": form_response.id})
@@ -34,33 +41,3 @@ def create_form_csv(form, investigation_slug, build_absolute_uri, io_object):
             print("Skipping row")
         except KeyError:
             print("Skipping row")
-
-
-def get_keys(form_instance: FormInstance):
-    keys = form_instance.json_properties
-    file_keys = _get_file_keys(form_instance)
-    non_file_fields = keys - file_keys
-    extra_fields = {"url", "version", "status", "email", "submission_date"}
-    fields = non_file_fields | {"meta_{}".format(field) for field in extra_fields}
-    return fields
-
-
-def _get_file_keys(form_instance: FormInstance):
-    # TODO: Signature does not work
-    file_widgets = ['signatureWidget', 'fileWidget']
-
-    file_keys = set()
-
-    ui_schema = form_instance.ui_schema_json
-    for step in form_instance.form_json:
-        schema = step["schema"]
-        for (property_name, property_values) in schema["properties"].items():
-            if ui_schema.get(schema["slug"], {}).get(property_name, {}).get("ui:widget") in file_widgets:
-                file_keys.add(property_name)
-            elif property_values.get("format") == "data-url":
-                file_keys.add(property_name)
-            elif property_values.get("type") == "array" \
-                    and property_values.get("items", {}).get("format") == "data-url":
-                file_keys.add(property_name)
-
-    return file_keys
