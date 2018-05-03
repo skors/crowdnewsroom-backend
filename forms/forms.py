3e9ddd36835755e2d3225994cbf997adfd5f05b5
Knut Hühne <knut.huehne@correctiv.org>
2018-05-03 16:44:14 +0200

    display new comment in same comment card style

--- forms/forms.py
+++ forms/forms.py
@@ -1,10 +1,14 @@
-from django.forms import ModelForm, MultipleChoiceField, CheckboxSelectMultiple
+from django.forms import ModelForm, CheckboxSelectMultiple, Textarea
 from django.utils import timezone
 
 from forms.models import Comment, FormResponse
 
 
 class CommentForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget = Textarea(attrs={'rows': 2, 'class': 'w-100'})
+
     class Meta:
         model = Comment
         fields = ["text"]
