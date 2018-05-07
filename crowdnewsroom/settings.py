8f6270779627dd2254f49a2e791ef8c6e1237eb8
Knut Hühne <knut.huehne@correctiv.org>
2018-05-07 17:01:13 +0200

    disable django-restframework's html view
    Django-Rest-Framework comes with a browsable API which renders information
    about the API when users are making request with their web
    browser directly instead of via AJAX.
    As we do not expect anyone to be using our API right
    now and do not include the required templates for these
    views anyways it makes sense to just turn it off for now.
    
    We might to turn it on back later as it can be quite useful.

--- crowdnewsroom/settings.py
+++ crowdnewsroom/settings.py
@@ -141,6 +141,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
