0f83ecd80c7912aea997eb5653769a4303de2896
Knut Hühne <knut.huehne@correctiv.org>
2018-05-22 18:22:17 +0200

    Manage django-webpack-loader in local_settings.py
    This was set to `not DEBUG` but `DEBUG` was only set later
    in the local_settings.py so this would always evaluate to
    `True`. It makes sense to make this explicit. To actually
    disable the cache when developing the following line should be added
    to `local_settings.py`:
    
    ```
    WEBPACK_LOADER["DEFAULT"]["CACHE"] = False
    ```

--- crowdnewsroom/settings.py
+++ crowdnewsroom/settings.py
@@ -166,8 +166,8 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'true'
 
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'CACHE': not DEBUG,
-        'BUNDLE_DIR_NAME': 'webpack_bundles/', # must end with slash
+        'CACHE': True,  # set to `False` in local_settings for development
+        'BUNDLE_DIR_NAME': 'webpack_bundles/',  # must end with slash
         'STATS_FILE': os.path.join(BASE_DIR, 'theme', 'webpack-stats.json'),
         'POLL_INTERVAL': 0.1,
         'TIMEOUT': None,
