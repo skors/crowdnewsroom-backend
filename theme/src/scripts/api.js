6b651d6e97e684c49223dc0a315370030985928e
Knut Hühne <knut.huehne@correctiv.org>
2018-06-14 16:42:18 +0200

    get csrf-cookie with js-cookie
    chrome and firefox store cookies slightly different
    and this seems like a safe abstraction

--- theme/src/scripts/api.js
+++ theme/src/scripts/api.js
@@ -1,17 +1,20 @@
-const authorizedFetch = (url, settings) => fetch(url, Object.assign({credentials: 'same-origin'}, settings)).then(response => response.json());
+import  Cookie  from "js-cookie";
+
+const authorizedFetch = (url, settings) =>
+  fetch(url, Object.assign({ credentials: "same-origin" }, settings)).then(
+    response => response.json()
+  );
 
 const authorizedPUT = (url, settings) => {
-  const csrfValue = document.cookie.split(';').find(c => c.indexOf("csrftoken") === 0).split("=")[1];
+  const csrfValue = Cookie.get("csrftoken");
   const baseSettings = {
     method: "PATCH",
     headers: {
-      'content-type': 'application/json',
-      'X-CSRFToken': csrfValue,
-    },
+      "content-type": "application/json",
+      "X-CSRFToken": csrfValue
+    }
   };
   return authorizedFetch(url, Object.assign(baseSettings, settings));
 };
 
-
-export {authorizedFetch,
-authorizedPUT}
+export { authorizedFetch, authorizedPUT };
