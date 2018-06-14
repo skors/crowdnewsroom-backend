77e29c315bb89b2bb160c23d93951d845d236409
Knut Hühne <knut.huehne@correctiv.org>
2018-06-14 17:39:49 +0200

    use carbon styles for comments

--- forms/forms.py
+++ forms/forms.py
@@ -1,13 +1,16 @@
-from django.forms import ModelForm, CheckboxSelectMultiple, Textarea
+from django.forms import ModelForm, Textarea
 from django.utils import timezone
 
-from forms.models import Comment, FormResponse, User
+from forms.models import Comment
+from django.utils.translation import ugettext_lazy as _
 
 
 class CommentForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['text'].widget = Textarea(attrs={'rows': 2, 'class': 'w-100'})
+        self.fields['text'].widget = Textarea(attrs={'rows': 2,
+                                                     'class': 'bx--text-area bx--text-area--v2',
+                                                     'placeholder': _('Add a comment')})
 
     class Meta:
         model = Comment
