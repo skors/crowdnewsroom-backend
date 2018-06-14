105bc21bc2f872876973e3424d19a8468ca8cfb3
Knut Hühne <knut.huehne@correctiv.org>
2018-06-14 17:57:29 +0200

    generate all bundles for production

--- theme/webpack.config-helper.js
+++ theme/webpack.config-helper.js
@@ -60,6 +60,11 @@ module.exports = options => {
 
   if (options.isProduction) {
     webpackConfig.entry = ["./src/scripts/index"];
+    webpackConfig.entry = {
+      main: "./src/scripts/index",
+      style : './src/styles/main.sass',
+      formResponseDetails : './src/scripts/form-response-details'
+    },
     webpackConfig.output.publicPath = "/static/js/";
 
     webpackConfig.plugins.push(
