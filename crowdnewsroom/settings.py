a67ad1f07d032388a78ccdc26521b0769d713595
Knut Hühne <knut.huehne@correctiv.org>
2018-06-18 11:10:01 +0200

    only offer session based auth for DRF in production setting

--- crowdnewsroom/settings.py
+++ crowdnewsroom/settings.py
@@ -139,11 +139,9 @@ LOGIN_REDIRECT_URL = "/forms/"
 LOGIN_URL = '/accounts/login'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication'
-    ),
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     )
