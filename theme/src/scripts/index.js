d93d4e8a7eda1ba35f4cb48dbac0ca23f12e3c22
Simon Jockers <hello@simonjockers.com>
2018-06-25 17:31:12 +0200

    Center user menu dropdown within component

--- theme/src/scripts/index.js
+++ theme/src/scripts/index.js
@@ -1,15 +1,15 @@
-'use strict'
+"use strict";
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
 
-import 'babel-polyfill'
+import "babel-polyfill";
 
-import "expose-loader?charts!./charts.js"
-import "./charts.js"
-import "./closest-polyfill.js"
+import "expose-loader?charts!./charts.js";
+import "./charts.js";
+import "./closest-polyfill.js";
 
-import { Tooltip, OverflowMenu } from 'carbon-components'
+import { Tooltip, OverflowMenu } from "carbon-components";
 Tooltip.init(document.body);
-OverflowMenu.init(document.body);
+OverflowMenu.init(document.body, { objMenuOffsetFlip: { top: 15, left: 0 } });
