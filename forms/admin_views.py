d29d60efb5ac485fd92c9b5941454012a580f51d
Knut Hühne <knut.huehne@correctiv.org>
2018-07-17 17:04:53 +0200

    only let people with manage_investigation permission download csvs

--- forms/admin_views.py
+++ forms/admin_views.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 from django.utils.functional import cached_property
 from guardian.decorators import permission_required
 from guardian.mixins import PermissionRequiredMixin
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, TemplateView, DeleteView
 from guardian.shortcuts import get_objects_for_user
@@ -326,14 +326,14 @@ def form_response_batch_edit(request, *args, **kwargs):
 
 
 @login_required(login_url="/admin/login")
-@permission_required('forms.view_investigation', (Investigation, 'slug', 'investigation_slug'), return_403=True)
+@permission_required('manage_investigation', (Investigation, 'slug', 'investigation_slug'), return_403=True)
 def form_response_csv_view(request, *args, **kwargs):
     form_slug = kwargs.get("form_slug")
     investigation_slug = kwargs.get("investigation_slug")
 
     form = get_object_or_404(Form, slug=form_slug)
     if form.investigation.slug != investigation_slug:
-        raise HttpResponse(status_code=403)
+        return HttpResponseForbidden()
 
     filter_params = _get_filter_params(kwargs, request.GET)
 
