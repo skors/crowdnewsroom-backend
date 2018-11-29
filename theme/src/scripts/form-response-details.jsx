4b79c87ca06f948a341c6ae73e2e5c64409f5d19
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-11-29 14:59:47 +0100

    Small frontend permission fixes

--- theme/src/scripts/form-response-details.jsx
+++ theme/src/scripts/form-response-details.jsx
@@ -5,24 +5,28 @@ import MultiSelectPicker from "./multiSelectPicker";
 
 
 const actionsWrapper = document.getElementsByClassName("formresponse-details--actions")[0];
-const id = actionsWrapper.getAttribute("data-response-id");
-const investigationSlug = actionsWrapper.getAttribute("data-investigation-slug");
 
-const bucketElement = document.getElementById("bucket-picker");
-render(<BucketPicker responseId={id} />, bucketElement);
+if (actionsWrapper) {
+  // due to permission rights this element doesn't exist for some users
+  const id = actionsWrapper.getAttribute("data-response-id");
+  const investigationSlug = actionsWrapper.getAttribute("data-investigation-slug");
 
-const assigneeElement = document.getElementById("assignee-picker");
-render(<MultiSelectPicker responseId={id}
-                          label={gettext("Add or delete ownership")}
-                          property="assignees"
-                          investigationSlug={investigationSlug}
-                          itemToString={item => `${item.first_name} ${item.last_name}`}
-                            />, assigneeElement);
+  const bucketElement = document.getElementById("bucket-picker");
+  render(<BucketPicker responseId={id} />, bucketElement);
 
-const tagsElement = document.getElementById("tag-picker");
-render(<MultiSelectPicker responseId={id}
-                          label={gettext("Add or delete tag")}
-                          property="tags"
-                          investigationSlug={investigationSlug}
-                          itemToString={item => item.name}
-/>, tagsElement);
+  const assigneeElement = document.getElementById("assignee-picker");
+  render(<MultiSelectPicker responseId={id}
+                            label={gettext("Add or delete ownership")}
+                            property="assignees"
+                            investigationSlug={investigationSlug}
+                            itemToString={item => `${item.first_name} ${item.last_name}`}
+                              />, assigneeElement);
+
+  const tagsElement = document.getElementById("tag-picker");
+  render(<MultiSelectPicker responseId={id}
+                            label={gettext("Add or delete tag")}
+                            property="tags"
+                            investigationSlug={investigationSlug}
+                            itemToString={item => item.name}
+  />, tagsElement);
+}
